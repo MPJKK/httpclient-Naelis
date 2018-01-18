2ace91e7947da29c2ca5b346dd99468315f5a55b
anukak <thenaelis@gmail.com>
2018-01-18 16:26:49 +0200

    Tehtävä tehty

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,6 +9,17 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
   tulos = 'Moro';
+  apitulos = '';
+  apiosoite = 'http://media.mw.metropolia.fi/wbma';
+  kuvaosoite = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  busosoite = 'http://api.digitransit.fi/realtime/vehicle-positions/v1/hfp/journey/+/+/2550/#';
+  testiosoite = 'http://api.hel.fi/linkedevents/v1/keyword/?format=json';
+  paikkaosoite = 'http://api.hel.fi/linkedevents/v1/place/?format=json';
+  eventtiosoite = 'http://api.hel.fi/linkedevents/v1/event/?include=location%2ckeywords/?format=json';
+  tulokset: any;
+  osoitteet: any;
+  eventit: any;
+
 
   constructor(private http: HttpClient) { }
 
@@ -25,9 +36,36 @@ export class HttpTestComponent implements OnInit {
 
   }
 
+/*  getFromApi() {
+
+        this.http.get(this.apiosoite + '/media').subscribe( data => {
+            console.log(data[0].filename);
+            this.apitulos = this.kuvaosoite + data[0].filename;
+        });
+    }*/
+
+    eventSearch() {
+
+        this.http.get(this.testiosoite).subscribe( data => {
+            console.log(data);
+            this.tulokset = data.data;
+        });
+
+        this.http.get(this.paikkaosoite).subscribe( data => {
+            console.log(data);
+            this.osoitteet = data.data;
+        });
+
+        this.http.get(this.eventtiosoite).subscribe( data => {
+            console.log(data);
+            this.eventit = data.data;
+        });
+    }
 
   ngOnInit() {
     this.getJson();
+//    this.getFromApi();
+    this.eventSearch();
   }
 
 }
